c18a958cca19ac47440e29edef8c4375a77fed1c
Oluwafemi Abolaji <abolajifemi85@gmail.com>
2026-07-29 08:08:52 +0100

    Code Enhancement

--- cypress/e2e/Femi/Artof_Testing/Login.cy.js
+++ cypress/e2e/Femi/Artof_Testing/Login.cy.js
@@ -1,9 +1,15 @@
-describe('Login Page', () => {
-
-  it('Opens the website', () => {
-
+describe('Example Cypress Site - Smoke Tests', () => {
+  // Visit the site before each test
+  beforeEach(() => {
     cy.visit('https://example.cypress.io')
-
   })
 
+  it('loads the homepage successfully', () => {
+    // assert the URL is correct
+    cy.url().should('include', 'example.cypress.io')
+
+    // basic content sanity check
+    cy.get('body').should('be.visible')
+    cy.contains('Kitchen Sink').should('exist')
+  })
 })
